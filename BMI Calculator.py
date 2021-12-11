b31dae6aceac4a9c2255087e8ea1a6ab99cf36ee
kishorprajapati1 <kishorprajapati416@gmail.com>
2021-12-11 14:02:52 +0530

    main

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,9 +1,15 @@
 #Body Mass Index Calculator
 print("BMI Calculator\n")
+
 # BMI formula= weight/height*height. 
 height=float(input("Enter the height in meters: "))
-weight=float(input("Enter the Weight is Kilograms: "))
-# Here height will be calculate first due to PEDMAS rule and then divided by weight and stored in bmi variable
-bmi=weight/(height*height)
+weight=int(input("Enter the Weight is Kilograms: "))
+
+# Here height will be calculate first due to PEDMAS rule and then divided by weight and stored in bmi_calc variable
+bmi_calc=weight/(height*height)
+
+# converting bmi_calc value to whole numbers
+bmi=int(bmi_calc)
+
 #Printing the result
-print("The BMI is: ",bmi)
+print("The BMI is: ",int(bmi))
