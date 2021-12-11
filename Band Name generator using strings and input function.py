37d94ce227608c30c5bd34b026784ce8bcaca7b1
kishorprajapati1 <kishorprajapati416@gmail.com>
2021-12-11 11:24:03 +0530

    Initial commit

---
+++ Band Name generator using strings and input function.py
@@ -0,0 +1,5 @@
+#Creating a band name and concatenating the city and pet name
+print("Welcome to the band name generator")#greeting
+city=input("Enter the city you grew up in? ")#city is variable and the user is asked to enter the input
+pet=input("Enter name of pet? ")#pet is variable and the user is asked to enter the input
+print("Your band name is"+" "+city+" "+pet)#here the value is concatenate as a strings
