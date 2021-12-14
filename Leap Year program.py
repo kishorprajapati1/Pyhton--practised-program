c4288d2164eecec110394f4a2649e9fd62a643f5
kishorprajapati1 <94631180+kishorprajapati1@users.noreply.github.com>
2021-12-14 22:38:19 +0530

    Leap Year Program
    Leap year program

--- Leap Year program.py
+++ Leap Year program.py
@@ -1,12 +1,13 @@
+#Program for Leap Year
 leap_year=int(input("Enter year to find the Leap Year. "))
 if leap_year%4==0:
     print("It is leap year.")
     if leap_year%100==0:
-        print("It is a leap year that is divided by 100")
+        print("It is a leap year.")
     elif leap_year%400==0:
         print("It is leap year divisible by 400")
     else:
-        print("It is not a leap year that is divisible by 100")
+        print("It is not a leap year.")
 
 else:
-    print("It is not leap year bitch, party hard.")
+    print("It is not leap year, party hard.")
